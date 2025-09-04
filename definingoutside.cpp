5cfe33df6c76dbee5a225f239c0e31de5d62fc39
suryapratap1121 <thakur.surya1303@gmail.com>
2025-09-04 14:47:25 +0530

    Create definingoutside.cpp

---
+++ definingoutside.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class student {
+    int rollno;
+    char name[50];
+    double fee;
+
+public:
+    student();
+    void display();
+};
+
+student::student() {
+    cout << "Enter the rollno: ";
+    cin >> rollno;
+    cout << "Enter the name: ";
+    cin >> name;
+    cout << "Enter the Fee: ";
+    cin >> fee;
+}
+
+void student::display() {
+    cout << "Roll No: " << rollno << endl;
+    cout << "Name: " << name << endl;
+    cout << "Fee: " << fee << endl;
+}
+
+int main() {
+    student s1;
+    s1.display();
+    return 0;
+}
